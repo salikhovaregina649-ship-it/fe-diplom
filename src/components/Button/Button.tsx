381ee4ac7189e897a7f811dbb861b4c8d8911397
Regina Salikhova <yanadera@mail.ru>
2026-02-19 16:29:58 +0400

    add component Button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,16 @@
+import "./Button.css";
+import React from "react";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
+    children: React.ReactNode;
+    variant?: "yellow" | "light" | "dark";
+    uppercase?: true | false;
+}
+
+export default function Button({children, className, variant = "yellow", uppercase = false, ...props}: ButtonProps) {
+    return(
+        <button className={`button button--${variant} ${className ?? ""} ${uppercase ? "button--uppercase" : ""}`} {...props}>
+            {children}
+        </button>
+    )
+}
