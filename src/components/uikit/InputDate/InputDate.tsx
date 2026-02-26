77acdb6058379dd6cc1cd2912199e6a5ce9c7dc5
Regina Salikhova <yanadera@mail.ru>
2026-02-26 16:42:10 +0400

    fixe search styles, added calendar developments

--- src/components/uikit/InputDate/InputDate.tsx
+++ src/components/uikit/InputDate/InputDate.tsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import Flatpickr from "react-flatpickr";
+import { Russian } from "flatpickr/dist/l10n/ru";
+import "flatpickr/dist/themes/light.css";
 import "./InputDate.css";
 import clsx from "clsx";
 
@@ -8,15 +12,38 @@ interface InputDateProps {
 }
 
 export default function InputDate({className, id, Icon}: InputDateProps) {
+    const [date, setDate] = useState<Date | null>(null);
+
+    // Ограничение: только текущий месяц
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
     return(
         <>
-            <input 
-                className={clsx("input-date", className)} 
-                id={id} 
-                type="date" 
-                placeholder="ДД.ММ.ГГ" 
+            <Flatpickr 
+                value={date ?? undefined}
+                onChange={(selectedDates) => setDate(selectedDates[0] ?? null)}
+                options={{
+                    dateFormat: "d.m.Y",
+                    monthSelectorType: "static",
+                    minDate: today,
+                    locale: Russian,
+                    prevArrow: '<span class="date-picker__custom-prev"></span>',
+                    nextArrow: '<span class="date-picker__custom-next"></span>',
+                }}
+                render={({ ...props }, ref) => (
+                    <div className="date-picker">
+                        <input
+                            {...props} ref={ref} 
+                            className={clsx("date-picker__input", className)} 
+                            id={id} 
+                            type="date" 
+                            placeholder="ДД.ММ.ГГ" 
+                        />
+                        {Icon && <Icon />}
+                    </div>
+                )}
             />
-            {Icon && <Icon />}
         </>
     )
 }
